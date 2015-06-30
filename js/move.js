NigoroJr/incog-neet

commit 76d27b20256a43b13dcc9eeee011189013dd79f0

[BUGFIX] ignore search events in function to move tabs
The bug is caused because the contextMenuMove function is called regardless of
whether 'move tab to...' or 'search Google for...' is shown. This issue does
not happen when moving a tab because the selected text is 'undefined'.

js/move.js
@@ -35,6 +35,10 @@ function moveTabToWindow(currentTab, windowId, incognito) {
 
 function contextMenuMove(info, currentTab) {
     var windowIdStr = info.menuItemId.replace('incog-neet#move#window', '');
+    // Search Google for... was clicked
+    if (!windowIdStr.match(/^incog-neet#move/)) {
+        return;
+    }
     var windowId = parseInt(windowIdStr);
 
     chrome.windows.get(windowId, function (win) {

commit 891f779bfcb65b384a9a62ba38dcacd4cc4d708a

[BUGFIX] 'move tab' does not do anything
This bug is due to the incorrect ID being matched. The window ID is looked at
when it should actually be the ID of the context menu item clicked. This
commit fixes the described bug introduced in
76d27b20256a43b13dcc9eeee011189013dd79f0

js/move.js
@@ -36,7 +36,7 @@ function moveTabToWindow(currentTab, windowId, incognito) {
 function contextMenuMove(info, currentTab) {
     var windowIdStr = info.menuItemId.replace('incog-neet#move#window', '');
     // Search Google for... was clicked
-    if (!windowIdStr.match(/^incog-neet#move/)) {
+    if (!info.menuItemId.match(/^incog-neet#move/)) {
         return;
     }
     var windowId = parseInt(windowIdStr);